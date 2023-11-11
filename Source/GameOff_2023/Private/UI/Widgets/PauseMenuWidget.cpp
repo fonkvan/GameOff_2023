4d660068ce0e9c7c4658c5c7e97e48dd67f0b25d
fonkvan <layedonovan@gmail.com>
2023-11-11 12:30:32 -0500

    Switched header file order due to compilation issues

--- Source/GameOff_2023/Private/UI/Widgets/PauseMenuWidget.cpp
+++ Source/GameOff_2023/Private/UI/Widgets/PauseMenuWidget.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Components/Button.h"
 #include "UI/Widgets/PauseMenuWidget.h"
+#include "Components/Button.h"
 
 void UPauseMenuWidget::NativeConstruct()
 {
