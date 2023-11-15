782c4b05619e99d32c9ca7ed5961c705bfd8cc40
fonkvan <layedonovan@gmail.com>
2023-11-15 16:18:08 -0500

    Initial commit of sliding update
    -First things first, clang-format changed. Why? Constructor Initializer lists. Previous format caused initializer lists to be all on one line. The problem with this is that some initializer lists, particularly the player in our case, can get quite long. It's best to have all on separate lines, so people don't have to scroll over. Strangely, it turns out ConstructorInitializerAllOnOneLineOrOnePerLine is actually deprecated, but the new setting (PackConstructorInitializers) doesn't actually work, so I just changed the current setting from true to false.
    
    -Sliding as a feature starting to be implemented. Can go into and out of slide state, capsule size decreases when sliding. There are no in animations in between sliding and not sliding, so it is a bit jarring.
    
    -Slide animation is from Animation Start Pack from Epic games. This was really not fun trying to figure out how to retarget this, and then actually getting it working (turns out it was actually fine for awhile, but it didn't look right because of an IK issues listed below)
    
    -Sliding animation hooked up in ABP. Had to remove a node that checks if we should do IK trace, as it ruins the sliding animation for some reason, regardless of what the bool that checks sliding returns. I want to look into this, but I don't think we really need IK for this project.
    
    -Reformatted files that used constructor initialization lists
    
    -Slow time input is fixed. Now only works on hold. The solution was one of two things: Change input type to "Hold" and change the start of slow time to Triggered instead of Start. The issue with this solution is "start slow time" would get called every 0.2 seconds, which is kind of spammy. My solution: keep button type as "Hold and Release", then change the start slow time to Started, and the end slow time to Canceled, and have the actuation time a very large number so that it never triggers.
    
    -GetTimeAbilityComponent made an inline function.

--- Source/GameOff_2023/Private/ActorComponents/TimeAbilityComponent.cpp
+++ Source/GameOff_2023/Private/ActorComponents/TimeAbilityComponent.cpp
@@ -5,7 +5,13 @@
 
 // Sets default values for this component's properties
 UTimeAbilityComponent::UTimeAbilityComponent()
-	: SlowAmount(0.5f), MaxMeterAmount(100.f), CurrentMeterAmount(100.f), MeterUseRate(0.05f), UseAmount(-1.5f), RefillAmount(0.5f), bAbilityActive(false)
+	: SlowAmount(0.5f)
+	, MaxMeterAmount(100.f)
+	, CurrentMeterAmount(100.f)
+	, MeterUseRate(0.05f)
+	, UseAmount(-1.5f)
+	, RefillAmount(0.5f)
+	, bAbilityActive(false)
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
